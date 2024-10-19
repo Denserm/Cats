4b881dc385b936743f6d738af079048447872f55
Andrey <maslakan78@gmail.com>
2024-10-19 14:38:02 +0300

    Добавлено изменение размера изображения

--- Cataas.py
+++ Cataas.py
@@ -13,13 +13,14 @@ def load_image(url):
         response.raise_for_status()
         image_data = BytesIO(response.content)
         img = Image.open(image_data)
+        img.thumbnail((600, 480), Image.Resampling.LANCZOS)
         return ImageTk.PhotoImage(img)
     except Exception as e:
         print(f"Произошла ошибка: {e}")
         return None
 
 
-def set_image()
+def set_image():
     img = load_image(url)
 
     if img:
@@ -28,7 +29,7 @@ def set_image()
 
 window = Tk()
 window.title("Cats!")
-window.geometry("600x480")
+window.geometry("600x520")
 
 label = Label()
 label.pack()
